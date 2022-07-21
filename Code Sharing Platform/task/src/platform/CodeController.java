af086fe4f1bd6900c063e818ec4bcfd9894297ae
gfton <gftonot@gmail.com>
2022-07-21 11:33:35 +0300

    Part 3

--- Code Sharing Platform/task/src/platform/CodeController.java
+++ Code Sharing Platform/task/src/platform/CodeController.java
@@ -2,81 +2,83 @@ package platform;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class CodeController {
-    private ConcurrentHashMap<Integer, Code> codes = new ConcurrentHashMap<>();
-    private String codeString = "public static void main(String[] args) {\n" +
+    private final ArrayList<Code> codes = new ArrayList<>();
+    private final String codeString = "public static void main(String[] args) {\n" +
             "        SpringApplication.run(CodeSharingPlatform.class, args);\n" +
             "    }";
     private Code code = new Code(codeString);
 
-    @GetMapping("/code")
-    public String getCode() {
-        String codeResponse = String.format("<html>\n" +
-                "<head>\n" +
-                "    <title>Code</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "    <span id='load_date'>%s</span>" +
-                "    <pre id='code_snippet'>\n" +
-                "%s" +
-                "</pre>\n" +
-                "</body>\n" +
-                "</html>", code.getDate(), code.getCode());
-        return codeResponse;
+    @GetMapping("/code/{id}")
+    public ModelAndView getCode(@PathVariable String id, Model model) {
+        model.addAttribute("code", codes.get(Integer.parseInt(id) - 1));
+        return new ModelAndView("code");
+    }
+
+    @GetMapping("/code/latest")
+    public ModelAndView getLatest(Model model) {
+        ArrayList<Code> sortedCodes = new ArrayList<>();
+        int maxLength = 10;
+
+        if (codes.size() < 10) {
+            maxLength = codes.size();
+        }
+
+        for (int i = codes.size() - 1; i >= Math.abs(codes.size()-maxLength); i--) {
+            sortedCodes.add(codes.get(i));
+        }
+
+        model.addAttribute("codes", sortedCodes);
+
+        return new ModelAndView("latest");
     }
 
     @GetMapping("/code/new")
-    public String addCode() {
-        String htmlResponse = "<html>\n" +
-                "<head>\n" +
-                "<script>" +
-                "function send() {\n" +
-                "    let object = {\n" +
-                "        \"code\": document.getElementById(\"code_snippet\").value\n" +
-                "    };\n" +
-                "    \n" +
-                "    let json = JSON.stringify(object);\n" +
-                "    \n" +
-                "    let xhr = new XMLHttpRequest();\n" +
-                "    xhr.open(\"POST\", '/api/code/new', false)\n" +
-                "    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');\n" +
-                "    xhr.send(json);\n" +
-                "    \n" +
-                "    if (xhr.status == 200) {\n" +
-                "      alert(\"Success!\");\n" +
-                "    }\n" +
-                "}" +
-                "</script>" +
-                "<title>Create</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<textarea id=\"code_snippet\"></textarea>"+
-                "<button id=\"send_snippet\" type=\"submit\" onclick=\"send()\">Submit</button>" +
-                "</body>\n" +
-                "</html>";
+    public ModelAndView addCode() {
 
-        return htmlResponse;
+        return new ModelAndView("addCode");
     }
 
-    @GetMapping("/api/code")
-    public Code getApiCode() {
-
-        return code;
+    @GetMapping("/api/code/{id}")
+    public Code getApiCode(@PathVariable String id) {
+        if (Integer.parseInt(id) - 1 < codes.size()) {
+            return codes.get(Integer.parseInt(id) - 1);
+        }
+        return null;
     }
 
     @PostMapping("/api/code/new")
     public String newCode(@RequestBody String jsonString) throws JsonProcessingException {
-        Map<String, String> codeValue =  new ObjectMapper().readValue(jsonString, Map.class);
-        code = new Code(codeValue.get("code"));
+        Map<String, String> codeValue = new ObjectMapper().readValue(jsonString, Map.class);
+        Code code = new Code(codeValue.get("code"));
+        codes.add(code);
+        this.code = code;
 
-        return "{}";
+        return String.format("{id:'%s'}", codes.size());
+    }
+
+    @GetMapping("/api/code/latest")
+    public ArrayList<Code> getApiLatest() {
+        ArrayList<Code> sortedCodes = new ArrayList<>();
+        int maxLength = 10;
+
+        if (codes.size() < 10) {
+            maxLength = codes.size();
+        }
+
+        for (int i = codes.size() - 1; i >= Math.abs(codes.size()-maxLength); i--) {
+            sortedCodes.add(codes.get(i));
+        }
+
+        return sortedCodes;
     }
 }
